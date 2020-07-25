e1576be151c10c43f2597771761a3419574485e7
sianblone <sianblone@gmail.com>
2020-07-25 23:52:21 +0900

    Update: Organize Imports

--- Community_Project/src/main/java/com/sif/community/model/BoardVO.java
+++ Community_Project/src/main/java/com/sif/community/model/BoardVO.java
@@ -2,7 +2,6 @@ package com.sif.community.model;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
--- Community_Project/src/main/java/com/sif/community/service/user/JoinService.java
+++ Community_Project/src/main/java/com/sif/community/service/user/JoinService.java
@@ -6,7 +6,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
